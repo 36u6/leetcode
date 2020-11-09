a15faee4011b7d7af42f84526764723148f2f088
36u6 <hoferbao@gmail.com>
2020-11-09 17:49:26 +0800

    dp

--- src/main/java/com/hofer/leetcode/p0053/Solution.java
+++ src/main/java/com/hofer/leetcode/p0053/Solution.java
@@ -5,4 +5,22 @@ package com.hofer.leetcode.p0053;
  * created on 2020/11/9 5:40 下午
  */
 public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new Solution().maxSubArray(nums));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
 }
